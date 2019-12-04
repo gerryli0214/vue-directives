896cb01d334bcff3c7381a4752951024134e0101
gerry <957282514@qq.com>
2019-12-04 17:21:07 +0800

    增加代码压缩

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel"
+import json from 'rollup-plugin-json'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default {
     input: 'src/index.js',
@@ -12,6 +14,8 @@ export default {
         resolve(),
         babel({
             exclude: 'node_modules/**'
-        })
+        }),
+        json(),
+        uglify()
     ]
 }
